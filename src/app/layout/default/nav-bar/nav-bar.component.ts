cc76033c0916cc4b2c5a90fb02be1a780d75888e
chenyuting <89172243+chenyuting12@users.noreply.github.com>
2024-01-16 14:34:42 +0800

    ✨ feat(update to do):

--- src/app/layout/default/nav-bar/nav-bar.component.ts
+++ src/app/layout/default/nav-bar/nav-bar.component.ts
@@ -24,7 +24,7 @@ import { toDoListLength } from '@app/config/constant';
   providers: [DestroyService]
 })
 export class NavBarComponent implements OnInit {
-  @Input() isMixiHead = false; 
+  @Input() isMixiHead = false;
   @Input() isMixiLeft = false;
   clientName: any = '';
   routerPath = this.router.url;
@@ -72,9 +72,9 @@ export class NavBarComponent implements OnInit {
       takeUntil(this.destroy$)
     );
 
-    
+
     this.subMixiModeSideMenu();
-    
+
     this.subIsCollapsed();
     this.subAuth();
     this.router.events
@@ -82,8 +82,8 @@ export class NavBarComponent implements OnInit {
         filter(event => event instanceof NavigationEnd),
         tap(() => {
           this.subTheme$.subscribe(() => {
-            
-            
+
+
             if (this.isMixiMode) {
               this.setMixModeLeftMenu();
             }
@@ -92,12 +92,12 @@ export class NavBarComponent implements OnInit {
           this.routerPath = this.activatedRoute.snapshot['_routerState'].url;
           this.clickMenuItem(this.menus);
           this.clickMenuItem(this.copyMenus);
-          
+
           if (this.isCollapsed && !this.isOverMode) {
             this.closeMenuOpen(this.menus);
           }
 
-          
+
           if (this.themesMode === 'top' && !this.isOverMode) {
             this.closeMenu();
           }
@@ -118,7 +118,7 @@ export class NavBarComponent implements OnInit {
         takeUntil(this.destroy$)
       )
       .subscribe(routeData => {
-        
+
         let isNewTabDetailPage = routeData['newTab'] === 'true';
 
         let route = this.activatedRoute;
@@ -136,7 +136,7 @@ export class NavBarComponent implements OnInit {
         );
         this.tabService.findIndex(this.routerPath);
         this.titleServe.setTitle(`UDPN-POC11`);
-        
+
         this.setMixModeLeftMenu();
       });
   }
@@ -154,7 +154,7 @@ export class NavBarComponent implements OnInit {
       });
   }
 
-  
+
   setMixModeLeftMenu(): void {
     this.menus.forEach(item => {
       if (item.selected) {
@@ -163,7 +163,7 @@ export class NavBarComponent implements OnInit {
     });
   }
 
-  
+
   cloneMenuArray(sourceMenuArray: Menu[], target: Menu[] = []): Menu[] {
     sourceMenuArray.forEach(item => {
       const obj: Menu = { menuName: '', menuType: 'C', path: '', id: -1, fatherId: -1 };
@@ -184,33 +184,33 @@ export class NavBarComponent implements OnInit {
     return target;
   }
 
-  
+
   changTopNav(index: number): void {
-    
+
     const currentTopNav = this.menus[index];
     let currentLeftNavArray = currentTopNav.children || [];
-    
+
     if (currentLeftNavArray.length > 0) {
-      
+
       currentLeftNavArray = currentLeftNavArray.filter(item => {
         return this.authCodeArray.includes(item.code!);
       });
-      
+
       if (currentLeftNavArray.length > 0 && !currentLeftNavArray[0].children) {
         this.router.navigateByUrl(currentLeftNavArray[0].path!);
       } else if (currentLeftNavArray.length > 0 && currentLeftNavArray[0].children) {
-        
+
         this.router.navigateByUrl(currentLeftNavArray[0].children[0].path!);
       }
 
-      
-      
-      
-      
-      
-      
-      
-      
+
+
+
+
+
+
+
+
     }
     this.splitNavStoreService.setSplitLeftNavArrayStore(currentLeftNavArray);
   }
@@ -239,7 +239,7 @@ export class NavBarComponent implements OnInit {
     this.cdr.markForCheck();
   }
 
-  
+
   changeOpen(currentMenu: Menu, allMenu: Menu[]): void {
     allMenu.forEach(item => {
       item.open = false;
@@ -267,20 +267,20 @@ export class NavBarComponent implements OnInit {
     this.router.navigate([menu.path]);
   }
 
-  
+
   subIsCollapsed(): void {
     this.isCollapsed$.subscribe(isCollapsed => {
       this.isCollapsed = isCollapsed;
-      
+
       if (!this.isCollapsed) {
         this.menus = this.cloneMenuArray(this.copyMenus);
         this.clickMenuItem(this.menus);
-        
+
         if (this.themesMode === 'mixi') {
           this.clickMenuItem(this.leftMenuArray);
         }
       } else {
-        
+
         this.copyMenus = this.cloneMenuArray(this.menus);
         this.closeMenuOpen(this.menus);
       }
@@ -301,7 +301,7 @@ export class NavBarComponent implements OnInit {
       .subscribe(res => (this.authCodeArray = res.authCode));
   }
 
-  
+
   private subMixiModeSideMenu(): void {
     this.leftMenuArray$.pipe(takeUntil(this.destroy$)).subscribe(res => {
       this.leftMenuArray = res;
@@ -309,13 +309,20 @@ export class NavBarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.total = this.windowSer.getSessionStorage(toDoListLength);
-    this.total = Number(this.total);
+    this.getToDoLength();
+    window.addEventListener('click', () => {
+      this.getToDoLength();
+    })
     this.subTheme$.subscribe(options => {
       if (options.mode === 'top' && !this.isOverMode) {
         this.closeMenu();
       }
     });
     this.clientName = sessionStorage.getItem('clientName');
+  }
+
+  getToDoLength() {
+    this.total = this.windowSer.getSessionStorage(toDoListLength);
+    this.total = Number(this.total);
   }
 }
