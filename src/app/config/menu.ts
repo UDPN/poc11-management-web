9693d8844fe9322f8d21dc0add29d8dbec277d18
chenyuting <chenyuting@reddatetech.com>
2024-12-17 10:26:37 +0800

    feat：update menu

--- src/app/config/menu.ts
+++ src/app/config/menu.ts
@@ -121,7 +121,7 @@ const menuNav: Menu[] = [
       {
         id: 2,
         fatherId: 7,
-        menuName: 'FX Service Provider Query',
+        menuName: 'FX Liquidity Provider Query',
         open: false,
         selected: false,
         menuType: 'C',
